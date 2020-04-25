44225635a87586cff05b6bd7288f35df7c633049
Vova-Nik <vmaseich@gmail.com>
2020-04-25 10:20:13 +0300

    dubble  effect fixed

--- getborders.js
+++ getborders.js
@@ -7,17 +7,19 @@ function init() {
     let bBtn = document.createElement("div");
     bBtn.innerHTML = 'Show blocks';
     document.body.appendChild(bBtn);
-    bBtn.style.backgroundColor = "rgba(200, 200, 200, .4)";
+    console.log("Created", bBtn)
+    bBtn.style.backgroundColor = "rgba(210, 210, 210, .6)";
     bBtn.style.border = "2px solid blue";
     bBtn.style.width = "150px";
     bBtn.style.textAlign = "center";
-    bBtn.style.textShadow = "1px 1px 0px rgba(216,216,216,0.8)";
-    bBtn.style.color = "rgba(20,20,20,0.9)";
+    bBtn.style.textShadow = "1px 1px 0px rgba(240, 240, 240, 0.6)";
+   
+
     bBtn.style.padding = "0 12px";
     bBtn.style.fontSize = "20px";
     bBtn.style.position = "fixed";
     bBtn.style.top = "256px";
-    bBtn.style.lef = "16px";
+    bBtn.style.left = "16px";
     bBtn.style.cursor = "pointer";
     bBtn.style.borderRadius = "6px";
     bBtn.className = "rendBtn";
@@ -28,9 +30,8 @@ document.addEventListener('DOMContentLoaded', init);
 
 //window.addEventListener(`resize`, event => {
 setInterval(() => {
-
+    console.log("width - ", window.screen.width);
     let bBtn = document.querySelector(".rendBtn");
     bBtn.innerHTML = `Show blocks  ${window.innerWidth}`;
 }
     , 600);
-
